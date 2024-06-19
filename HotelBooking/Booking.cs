87c6404471a49f3ff8e7b2ee10bab1024729ebfe
iamnana-tyc <nanatyc7@gmail.com>
2024-06-19 16:11:13 +0500

    added new attributes

--- HotelBooking/Booking.cs
+++ HotelBooking/Booking.cs
@@ -1,25 +1,29 @@
 using System.Dynamic;
 
-public class Booking {
-
-//  Booking (guest name, room number, check in date, check out date)
-
-public string GuestName {get; set;}
-//public int RoomNumber {get; set;}
-
-public Room Room {get; set;}
-public DateTime CheckInDate {get; set;}
-public DateTime CheckOutDate {get; set;}
-
-public Booking(string GuestName, Room room, DateTime CheckInDate, DateTime CheckOutDate)
+public class Booking
 {
-    this.GuestName = GuestName;
-    this.Room = room;
-    this.CheckInDate = CheckInDate;
-    this.CheckOutDate = CheckOutDate;    
-}
 
-public override string ToString()
+    //  Booking (guest name, room number, check in date, check out date)
+
+    public string GuestName { get; set; }
+
+    public Room Room { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+
+    public int Duaration => (CheckOutDate - CheckInDate).Days;
+
+
+
+    public Booking(string GuestName, Room room, DateTime CheckInDate, DateTime CheckOutDate)
+    {
+        this.GuestName = GuestName;
+        this.Room = room;
+        this.CheckInDate = CheckInDate;
+        this.CheckOutDate = CheckOutDate;
+    }
+
+    public override string ToString()
     {
         return $"GuestName: {GuestName}, RoomNumber: {Room}, from {CheckInDate.ToShortDateString()} to {CheckOutDate.ToShortDateString()}";
     }
