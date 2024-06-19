0e8461fd99b90febdb1e6d215fd6aafe2957aa40
iamnana-tyc <nanatyc7@gmail.com>
2024-06-19 17:10:52 +0500

    third-commit

--- HotelBooking/obj/Debug/net8.0/HotelBooking.AssemblyInfo.cs
+++ HotelBooking/obj/Debug/net8.0/HotelBooking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc6bc740d643b96e59077d4985ca3d2f5ffecf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cbab9577ac3fefc427a1a39ae0b9323b5531dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
