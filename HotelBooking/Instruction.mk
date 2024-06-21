729e81d8ab515b6cebf92a16af8266f53e34b480
iamnana-tyc <nanatyc7@gmail.com>
2024-06-21 15:06:30 +0500

    Added new task instructions

--- HotelBooking/Instruction.mk
+++ HotelBooking/Instruction.mk
@@ -58,3 +58,39 @@ Recommendations:
 - Make a separate sub-menu for queries
 - Create a separate method for each query. You may (or may not - if you got better ideas) put them in a separate static class "Queries".
 - Use IEnumerable where possible instead of List (you may start with List, but then change your methods to receive/return IEnumerable instead; at any time you may convert IEnumerable to List with .ToList() method).
+
+
+Improvements:
+- Input methods better stay in their own static class (like Input, and be used like Input.InputString(...)), not in BookingService - cuz they are not done specifically for this service, they may be used everywhere throughout your console application
+- Query methods better stay in their own static class (like Query and be used like Query.AllBookings(...)).
+- Query methods better take List of rooms/bookings as input and also return a List of rooms/bookings that aligned with your filter (or just return an integer, if a sum is calculated, for example). 
+This way you separate business login in separate method and print the result afterwards - good programming is done this way (after some time you will understand from experience why it is done like this - it makes code more flexible). 
+But your LINQs are not that complex at the moment, it is not really that necessary. But remember separating business logic is good.
+
+Next task:
+Expand the existing Room and Booking model by implementing an interface and using class inheritance. This task will help you understand and practice the concepts of interfaces and inheritance in C#.
+
+1. Create an Interface: IIdentifiable
+- Define an interface named IIdentifiable.
+- This interface should include a single property Id of type int.
+- Both Room and Booking classes should implement this interface.
+
+2. Create a Base Class: BaseEntity
+- Create a base class named BaseEntity.
+- This class should implement the IIdentifiable interface.
+- Include an Id property of type int in this class.
+- Both Room and Booking classes should inherit from BaseEntity.
+
+3. Implement a Display Method:
+- In the IIdentifiable class, add a method Display() - we require all classes that implement this interface to have this method
+- In the BaseEntity class, override Display() method and return the name of the class (this.GetType().Name) with its Id.
+- In the Room and Booking classes override Display() to display additional information (e.g., RoomNumber for Room, and StartDate, EndDate for Booking) along with name of the class and Id.
+
+4. Testing base entities:
+- Create a list of BaseEntity that would include all your Rooms and Bookings; add a method to main program to print all created entities
+- Iterate over instances of BaseEntity and call Display() method. Explain the results (why it includes specific properties or not?)
+
+5. Testing interfaces:
+- Create a list of IIdentifiable objects in the Main method.
+- Add instances of Room and Booking to this list.
+- Iterate through the list and call the Display() method on each item to demonstrate late binding. Explain the results (why it includes specific properties or not?)
