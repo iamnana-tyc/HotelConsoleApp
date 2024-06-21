d337b524881dc391dca6d52b492c1b9a97346011
iamnana-tyc <nanatyc7@gmail.com>
2024-06-22 01:40:39 +0500

    implemented demo samples

--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -1,117 +1,189 @@
-﻿class Program
+﻿using System.Data.Common;
+
+class Program
 {
     static void Main(string[] args)
     {
+        List<IIdentifiable> identifiableObjects = new List<IIdentifiable>();
+
+        List<BaseEntity> baseEntities = new List<BaseEntity>();
 
         BookingService bookingService = new BookingService();
-        bool appStarts = true;
 
 
-        // Adding some rooms to the booking service
+
+        //  bool appStarts = true;
+
+        Query.Initialiaze(bookingService.Rooms, bookingService.Bookings);
+
+        // Adding rooms
         Room room1 = new Room(100, 150m, 2);
         Room room2 = new Room(200, 250m, 4);
         Room room3 = new Room(300, 400m, 5);
+
 
         bookingService.AddARoom(room1);
         bookingService.AddARoom(room2);
         bookingService.AddARoom(room3);
 
-        // Adding some booking 
+        // booking a room
         Booking booking1 = new Booking(GuestName: "Tommy", room: room1, CheckInDate: new DateTime(2024, 7, 1), CheckOutDate: new DateTime(2024, 7, 6));
         Booking booking2 = new Booking(GuestName: "Evans", room: room2, CheckInDate: new DateTime(2024, 7, 11), CheckOutDate: new DateTime(2024, 7, 20));
         Booking booking3 = new Booking(GuestName: "John", room: room1, CheckInDate: new DateTime(2024, 7, 8), CheckOutDate: new DateTime(2024, 7, 11));
         Booking booking4 = new Booking(GuestName: "Gaby", room: room3, CheckInDate: new DateTime(2024, 7, 20), CheckOutDate: new DateTime(2024, 7, 30));
+
 
         bookingService.BookAHotel(booking1);
         bookingService.BookAHotel(booking2);
         bookingService.BookAHotel(booking3);
         bookingService.BookAHotel(booking4);
         /*
-        bookingService.AddARoom(new Room(100, 150m, 2));
-        bookingService.AddARoom(new Room(200, 250m, 4));
-        bookingService.AddARoom(new Room(300, 350m, 5));
-        */
 
-        bookingService.FindLeastPricedRoom();
+                // adding rooms to the baseentities
+                baseEntities.Add(room1);
+                baseEntities.Add(room2);
+                baseEntities.Add(room3);
 
-        Console.WriteLine();
+                // adding booking to baseentities
 
-        bookingService.ShowAverageComfortLevel();
+                baseEntities.Add(booking1);
+                baseEntities.Add(booking2);
+                baseEntities.Add(booking3);
+                baseEntities.Add(booking4);
 
+                // display entities
+                DisplayEntities(baseEntities);
 
-        Console.WriteLine();
-        Console.WriteLine("Welcome to Nana Resort Hotel");
-
-
-        while (appStarts)
-        {
-            Console.WriteLine("Main menu:");
-            Console.WriteLine("1. Book a Room");
-            Console.WriteLine("2. List all Booking");
-            Console.WriteLine("3. List all available Rooms");
-            Console.WriteLine("4. show query menu");
-            Console.WriteLine("5. Exit");
-            Console.WriteLine("Select an option from either: 1,2,3,4,5");
-
-            string input = Console.ReadLine();
-            switch (input)
-            {
-                case "1":
-                    Booking booking = CreateABooking(bookingService);
-                    if (booking != null)
-                    {
-                        bookingService.BookAHotel(booking);
+                // a method to print all entities
+                static void DisplayEntities(List<BaseEntity> baseEntities){
+                    foreach(var entities in baseEntities){
+                        Console.WriteLine(entities.Display());
                     }
-                    break;
 
-                case "2":
-                    bookingService.ListAllBooking();
-                    break;
+                }
 
-                case "3":
-                    bookingService.ListRooms();
-                    break;
-                case "4":
-                    ShowQueryMenu(bookingService);
-                    break;
-                case "5":
-                    appStarts = false;
-                    Console.WriteLine("You have exit the app");
-                    break;
+                 // EXPLANATION
+                 // we are able to display the propeties of the Room and book using the display method in the base Entity class.
+                 // Both classes are using the same Display method, however at run time the Display method display the properties that are 
+                 // relevant to the actual object. So in this case:
+                 // the display method displays room number and price, whereas for the Booking it displays 
+                 // check-in date, check-out and duration. This 
+                 // Both sub classes are using the same display method yet at run time, the display method displays the 
+                 // propeties relevant to the class
+            */
 
-                default:
-                    Console.WriteLine("Invalid input, enter the correct option");
-                    break;
+        // adding rooms to the indentifiableobject
+        identifiableObjects.Add(room1);
+        identifiableObjects.Add(room2);
+        identifiableObjects.Add(room3);
+
+        // adding booking to identifiable list
+        identifiableObjects.Add(booking1);
+        identifiableObjects.Add(booking2);
+        identifiableObjects.Add(booking3);
+        identifiableObjects.Add(booking4);
+
+        DisplayEntities(identifiableObjects);
+
+        static void DisplayEntities(List<IIdentifiable> identifiables)
+        {
+            foreach (var identifiable in identifiables)
+            {
+                Console.WriteLine(identifiable.Display());
             }
         }
-        Console.WriteLine();
 
-        bookingService.LongBookingLessThan7Days();
+        // EXPLANATION
+        // The interface which is IIdentifiable has a method call Display. This Superclass makes sure that 
+        // both Room and Booking which are all subclasses implement the display method. 
+        // However during run time the Display method shows different output based on the object type 
+        // so we see that for Room Display method shows Room number and price, and for Booking,
+        // it shows the check-in date, check-out and duration even though they both running the same Display method.  
 
-        Console.WriteLine();
 
-        bookingService.NumberOfDaysBookedPerBooking();
 
-        Console.WriteLine();
 
-        bookingService.TotalRevenueForEachRoom();
+        //     Query.FindLeastPricedRoom();
+
+        //     Console.WriteLine();
+
+        //     Query.ShowAverageComfortLevel();
+
+
+        //     Console.WriteLine();
+        //     Console.WriteLine("Welcome to Nana Resort Hotel");
+        /*
+
+                while (appStarts)
+                {
+                    Console.WriteLine("Main menu:");
+                    Console.WriteLine("1. Book a Room");
+                    Console.WriteLine("2. List all Booking");
+                    Console.WriteLine("3. List all available Rooms");
+                    Console.WriteLine("4. show query menu");
+                    Console.WriteLine("5. Exit");
+                    Console.WriteLine("Select an option from either: 1,2,3,4,5");
+
+                    string input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            Booking booking = CreateABooking(bookingService);
+                            if (booking != null)
+                            {
+                                bookingService.BookAHotel(booking);
+                            }
+                            break;
+
+                        case "2":
+                            Query.ListAllBooking();
+                            break;
+
+                        case "3":
+                            Query.ListRooms();
+                            break;
+                        case "4":
+                            ShowQueryMenu(bookingService);
+                            break;
+                        case "5":
+                            appStarts = false;
+                            Console.WriteLine("You have exit the app");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid input, enter the correct option");
+                            break;
+                    }
+                }
+                Console.WriteLine();
+
+                Query.LongBookingLessThan7Days();
+
+                Console.WriteLine();
+
+                Query.NumberOfDaysBookedPerBooking();
+
+                Console.WriteLine();
+
+                Query.TotalRevenueForEachRoom();
+           */
 
     }
 
     static Booking? CreateABooking(BookingService bookingService)
     {
-        bookingService.ListAllBooking();
+        Query.ListAllBooking();
 
-        int roomNumber = bookingService.InputInt("Enter room number: ", 100, 300);
-        Room room = bookingService.FindARoom(roomNumber);
+        int roomNumber = Input.InputInt("Enter room number: ", 100, 300);
+        Room room = Query.FindARoom(roomNumber);
         if (room == null)
         {
             Console.WriteLine("There is no such room");
             return null;
         }
-        string guestName = bookingService.InputString("Enter the guest name: ");
-        DateTime checkInDate = bookingService.InputDateTime("Enter check-in date (yyy-mm-dd): ");
-        DateTime checkOutDate = bookingService.InputDateTime("Enter check-out date (yyy-mm-dd): ");
+        string guestName = Input.InputString("Enter the guest name: ");
+        DateTime checkInDate = Input.InputDateTime("Enter check-in date (yyy-mm-dd): ");
+        DateTime checkOutDate = Input.InputDateTime("Enter check-out date (yyy-mm-dd): ");
 
         return new Booking(guestName, room, checkInDate, checkOutDate);
     }
@@ -136,7 +208,7 @@
                     Console.WriteLine("Enter the room number");
                     if (int.TryParse(Console.ReadLine(), out int roomNumber))
                     {
-                        bookingService.ListOfAllBookingForSpecificRoom(roomNumber);
+                        Query.ListOfAllBookingForSpecificRoom(roomNumber);
                     }
                     else
                         Console.WriteLine("The room number does't exist.");
@@ -145,13 +217,13 @@
                     Console.Write("Enter the dates (yyy-mm-dd): ");
                     if (DateTime.TryParse(Console.ReadLine(), out DateTime date))
                     {
-                        bookingService.AllRoomsFreeAtSpecificDay(date);
+                        Query.AllRoomsFreeAtSpecificDay(date);
                     }
                     else
                         Console.WriteLine("Invalid input dates, please enter correct dates.");
                     break;
                 case "3":
-                    bookingService.TotalRevenueForAllRooms();
+                    Query.TotalRevenueForAllRooms();
                     break;
                 case "4":
                     queryMenuOnly = false;
@@ -162,7 +234,6 @@
                     break;
 
             }
-
 
         }
 
